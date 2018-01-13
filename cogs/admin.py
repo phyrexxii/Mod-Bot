a3220f9bf4b49df4d2869ed9321fd71336097ca7
Sam <34219071+RageKickedGamer@users.noreply.github.com>
2018-01-12 18:12:42 -0800

    Fixed prefix

--- cogs/admin.py
+++ cogs/admin.py
@@ -8,7 +8,7 @@ import asyncio
 import time
 import datetime
 
-bot = commands.Bot(command_prefix = "m.")
+bot = commands.Bot(commands_prefix = when_mentioned_or ("m."))
 
 version = "Mod Bot v0.1"
 
